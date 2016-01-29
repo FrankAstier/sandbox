e33066bb3a6b6cda4227c77c16555905a01c0c20
Frank Astier <fastier@linkedin.com>
2016-01-29 13:34:13 -0800

    WIP LH PS2

--- linear-hash/include/linear-hash-PS2.hpp
+++ linear-hash/include/linear-hash-PS2.hpp
@@ -21,81 +21,91 @@ template <typename K, typename V>
 struct LinearHash_PS2 {
 
   LinearHash_PS2(size_t n_buckets =100, size_t bucket_size =4, bool debug = false)
-  : buckets(n_buckets),
-    priority(),
-    not_split(buckets.size(), 1),
-    m(n_buckets),
-    b(bucket_size),
-    i(0),
-    n_entries(0),
-    search_length(0),
-    debug(debug)
-  {}
+      : buckets(n_buckets),
+        priority(),
+        not_split(),
+        m(n_buckets),
+        b(bucket_size),
+        i(0),
+        n_entries(0),
+        n_probes(0),
+        debug(debug)
+  {
+    for (size_t ii = 0; ii < buckets.size(); ++i)
+      not_split.insert(ii);
+  }
 
   void put(const K& key, const V&value) {
 
-//    ++n_entries;
-//    size_t idx = bucket_index(key, i);
-//
-//    buckets[idx].push_back({key, value});
-//
-//    if (buckets[idx].size() > b && not_split[idx])
-//      priority.push_back(idx);
-//
-//    sort(priority.begin(), priority.end(), order);
-//
-//    float lf = (float) n_entries / (float) buckets.size();
-//
-//    if (lf > .95) {
-//
-//      size_t to_split = priority.back() < (1 << i) * m ? priority.back() : *not_split.begin();
-//      priority.resize(priority.size() - 1);
-//      not_split.erase(to_split);
-//
-//      size_t new_idx = to_split + (1 << i) * m;
-//
-//      if (new_idx > buckets.size())
-//        buckets.resize(new_idx + 1);
-//
-//      Bucket old_bucket, new_bucket = buckets[new_idx];
-//
-//      for (auto kv : buckets[to_split]) {
-//
-//        if (new_idx == to_split)
-//          old_bucket.push_back(kv);
-//        else
-//          new_bucket.push_back(kv);
-//      }
-//
-//      buckets[idx].swap(old_bucket);
-//
-//      p = *not_split.begin();
-//      if (p == (1 << i) * m) {
-//        p = 0;
-//        ++i;
-//        size_t new_size = (1 << (i+1)) * m;
-//        not_split.resize(new_size);
-//        fill(not_split.begin(), not_split.end(), 1);
-//      }
-//    }
+    ++n_entries;
+    size_t idx = bucket_index(key, i);
+    buckets[idx].push_back({key, value});
+
+    if (buckets[idx].size() > b && not_split.count(idx)) {
+      priority.push_back(idx);
+      sort(priority.begin(), priority.end(),
+           [&](size_t a, size_t b) { return buckets[a].size() < buckets[b].size(); });
+    }
+
+    float lf = (float) n_entries / (float) buckets.size();
+
+    if (lf > .95) {
+
+      size_t to_split = numeric_limits<size_t>::max();
+
+      if (priority.back() < (1 << i) * m && not_split.count(idx)) {
+        to_split = priority.back();
+        priority.resize(priority.size() - 1);
+      } else if (!not_split.empty())
+        to_split = *not_split.begin();
+
+      assert(to_split != numeric_limits<size_t>::max());
+
+      not_split.erase(to_split);
+      size_t new_bucket_idx = to_split + (1 << i) * m;
+
+      if (new_bucket_idx > buckets.size())
+        buckets.resize(new_bucket_idx + 1);
+
+      Bucket old_bucket, new_bucket = buckets[new_bucket_idx];
+
+      for (auto kv : buckets[to_split]) {
+        size_t h = utils::fasthash64(key);
+        size_t k = (1 << (i+1)) * m;
+        size_t new_idx = h % k;
+        if (new_idx == to_split)
+          old_bucket.push_back(kv);
+        else
+          new_bucket.push_back(kv);
+      }
+
+      buckets[idx].swap(old_bucket);
+
+      if (not_split.empty()) {
+        ++i;
+        assert(buckets.size() == (1 << i) * m);
+        for (size_t ii = 0; ii < buckets.size(); ++i)
+          not_split.insert(ii);
+      }
+    }
   }
 
   pair<bool, V> get(const K& key) {
     size_t idx = bucket_index(key, i);
     for (size_t j = 0; j < buckets[idx].size(); ++j) {
       if (buckets[idx][j].first == key) {
-        search_length = ((n_entries-1) * search_length + j)/n_entries;
+        n_probes = ((n_entries-1) * n_probes + j)/n_entries;
         return {true, buckets[idx][j].second};
       }
     }
     return {false, V()};
   }
 
-  size_t bucket_index(const K& key, size_t ii) const {
+  size_t bucket_index(const K& key, size_t ii) {
     size_t h = utils::fasthash64(key);
     size_t k = (1 << ii) * m;
     size_t hm = h % k;
-    if (not_split[hm])
+    if (not_split.count(hm))
       return hm;
     return h % (2*k);
   }
@@ -129,7 +139,7 @@ struct LinearHash_PS2 {
     << " min:" << min_bucket_occupancy
     << " avg:" << (avg_bucket_occupancy/(buckets.size() - n_empty_buckets))
     << " max:" << max_bucket_occupancy
-    << " len:" << search_length // number of probes to reach a record or not present
+    << " len:" << n_probes // number of probes to reach a record or not present
     << endl;
 
     vector<float> dist((size_t)max_bucket_occupancy+1);
@@ -154,20 +164,14 @@ struct LinearHash_PS2 {
 
   typedef vector<pair<K,V>> Bucket;
 
-  function<bool(size_t,size_t)> order = [&](size_t a, size_t b) {
-    return buckets[a].size() < buckets[b].size();
-    //return !((a < p && b >= p)
-    //       || (a < p && b < p && buckets[a].size() > buckets[b].size()));
-  };
-
   vector<Bucket> buckets;
   vector<size_t> priority;
-  vector<bool> not_split;
+  set<size_t> not_split;
   const size_t m; // initial number of buckets
   const size_t b; // bucket size
   size_t i; // splitting round number
   size_t n_entries; // total number of entries in hash
-  float search_length;
+  float n_probes;
   bool debug;
 };
 
