cd145f0fa397f5b50062bab8fb55535ffd2535fc
Frank Astier <fastier@yahoo.com>
2016-01-29 09:58:10 -0800

    WIP Linear hash with priority splitting

--- linear-hash/include/linear-hash-PS2.hpp
+++ linear-hash/include/linear-hash-PS2.hpp
@@ -9,100 +9,79 @@
 #include <stl_io.hpp>
 
 using namespace utils;
+using namespace std;
 
 /**
  * Linear hash with priority splitting - take 2: splitting with priority.
+ *
+ * linear probe
+ * split or fill holes in zone 3? just split first
  */
 template <typename K, typename V>
 struct LinearHash_PS2 {
 
   LinearHash_PS2(size_t n_buckets =100, size_t bucket_size =4, bool debug = false)
-      : buckets(n_buckets),
-        priority(),
-        m(n_buckets),
-        b(bucket_size),
-        i(0),
-        p(0),
-        n_entries(0),
-        n_hits(0),
-        search_length(0),
-        debug(debug)
+  : buckets(n_buckets),
+    priority(),
+    not_split(buckets.size(), 1),
+    m(n_buckets),
+    b(bucket_size),
+    i(0),
+    n_entries(0),
+    search_length(0),
+    debug(debug)
   {}
 
   void put(const K& key, const V&value) {
-    if (debug) std::cout << "Inserting " << key << " " << value << " - ";
-    if (debug) std::cout << "m= " << m << " b= " << b << " i= " << i << " p= " << p;
-    if (debug) std::cout << " n buckets= " << buckets.size() << std::endl;
-    ++n_entries;
-    uint64_t idx = bucket_index(key, i);
-    assert(idx < buckets.size());
-    assert(p <= (1 << i) * m);
-    assert(buckets.size() == (1 << i) * m + p);
-    if (debug) std::cout << "\tStoring " << key << "," << value << " in bucket: " << idx << std::endl;
-    buckets[idx].push_back({key, value});
 
-    if (buckets[idx].size() > b) {
-      priority.push_back(idx);
-      std::make_heap(priority.begin(), priority.end(), order);
-    }
-
-    float lf = (float) n_entries / (float) buckets.size();
-
-    if (lf > .95 && *priority.begin() < p) {
-      size_t to_split = priority.back();
-      priority.resize(priority.size() - 1);
-      ++n_hits;
-      if (debug) std::cout << "\tSplitting bucket " << to_split << std::endl;
-      Bucket old_bucket;
-      for (auto kv : buckets[to_split]) {
-        uint64_t h = utils::fasthash64(key);
-        uint64_t k = (1 << (i+1)) * m;
-        uint64_t new_idx = h % k;
-        Bucket new_bucket = buckets[new_idx];
-        if (debug) std::cout << "\tkey: " << kv.first;
-        if (debug) std::cout << " old index= " << bucket_index(kv.first, i);
-        if (debug) std::cout << " -> new index= " << new_idx << " ";
-        assert(new_idx <= buckets.size());
-        assert(new_idx == to_split || new_idx == to_split + (1 << i) * m);
-        if (new_idx == to_split) {
-          if (debug) std::cout << "\t\t" << kv << " stays in bucket " << to_split << std::endl;
-          old_bucket.push_back(kv);
-        } else {
-          if (debug) std::cout << "\t\t" << kv << " goes to new bucket " << new_idx << std::endl;
-          new_bucket.push_back(kv);
-        }
-      }
-      buckets[idx].swap(old_bucket);
-
-    } else if (buckets[idx].size() > b) {
-      if (debug) std::cout << "\tOverflow on bucket " << idx << " - Splitting bucket " << p << std::endl;
-      Bucket old_bucket, new_bucket;
-      for (auto kv : buckets[p]) {
-        uint64_t new_idx = bucket_index(kv.first, i+1);
-        if (debug) std::cout << "\tnew index= " << new_idx << " ";
-        assert(new_idx <= buckets.size());
-        assert(new_idx == p || new_idx == p + (1 << i) * m);
-        if (new_idx == p) {
-          if (debug) std::cout << "\t\t" << kv << " stays in bucket " << p << std::endl;
-          old_bucket.push_back(kv);
-        } else {
-          if (debug) std::cout << "\t\t" << kv << " goes to new bucket " << new_idx << std::endl;
-          new_bucket.push_back(kv);
-        }
-      }
-      buckets[p].swap(old_bucket);
-      buckets.push_back(new_bucket);
-      ++p;
-      if (p == (1 << i) * m) {
-        if (debug) std::cout << "\tp = " << p << " going to next round" << std::endl;
-        p = 0;
-        ++i;
-      }
-    }
+//    ++n_entries;
+//    size_t idx = bucket_index(key, i);
+//
+//    buckets[idx].push_back({key, value});
+//
+//    if (buckets[idx].size() > b && not_split[idx])
+//      priority.push_back(idx);
+//
+//    sort(priority.begin(), priority.end(), order);
+//
+//    float lf = (float) n_entries / (float) buckets.size();
+//
+//    if (lf > .95) {
+//
+//      size_t to_split = priority.back() < (1 << i) * m ? priority.back() : *not_split.begin();
+//      priority.resize(priority.size() - 1);
+//      not_split.erase(to_split);
+//
+//      size_t new_idx = to_split + (1 << i) * m;
+//
+//      if (new_idx > buckets.size())
+//        buckets.resize(new_idx + 1);
+//
+//      Bucket old_bucket, new_bucket = buckets[new_idx];
+//
+//      for (auto kv : buckets[to_split]) {
+//
+//        if (new_idx == to_split)
+//          old_bucket.push_back(kv);
+//        else
+//          new_bucket.push_back(kv);
+//      }
+//
+//      buckets[idx].swap(old_bucket);
+//
+//      p = *not_split.begin();
+//      if (p == (1 << i) * m) {
+//        p = 0;
+//        ++i;
+//        size_t new_size = (1 << (i+1)) * m;
+//        not_split.resize(new_size);
+//        fill(not_split.begin(), not_split.end(), 1);
+//      }
+//    }
   }
 
-  std::pair<bool, V> get(const K& key) {
-    uint64_t idx = bucket_index(key, i);
+  pair<bool, V> get(const K& key) {
+    size_t idx = bucket_index(key, i);
     for (size_t j = 0; j < buckets[idx].size(); ++j) {
       if (buckets[idx][j].first == key) {
         search_length = ((n_entries-1) * search_length + j)/n_entries;
@@ -112,82 +91,82 @@ struct LinearHash_PS2 {
     return {false, V()};
   }
 
-  uint64_t bucket_index(const K& key, size_t ii) const {
-    uint64_t h = utils::fasthash64(key);
-    uint64_t k = (1 << ii) * m;
-    uint64_t hm = h % k;
-    uint64_t h2 = h % (2*k);
-    return hm >= p ? hm : h2;
+  size_t bucket_index(const K& key, size_t ii) const {
+    size_t h = utils::fasthash64(key);
+    size_t k = (1 << ii) * m;
+    size_t hm = h % k;
+    if (not_split[hm])
+      return hm;
+    return h % (2*k);
   }
 
   void print() const {
     size_t n_empty_buckets = 0;
     float avg_bucket_occupancy = 0;
-    float min_bucket_occupancy = std::numeric_limits<float>::max();
+    float min_bucket_occupancy = numeric_limits<float>::max();
     float max_bucket_occupancy = 0;
 
     for (size_t j = 0; j < buckets.size(); ++j) {
-      if (debug) std::cout << "\tBucket " << j;
+      if (debug) cout << "\tBucket " << j;
       if (!buckets[j].empty()) {
-        if (debug) std::cout << " size= " << buckets[j].size() << ": ";
+        if (debug) cout << " size= " << buckets[j].size() << ": ";
         for (auto kv : buckets[j])
-          if (debug) std::cout << kv << " ";
-        if (debug) std::cout << std::endl;
-        min_bucket_occupancy = std::min(min_bucket_occupancy, (float) buckets[j].size());
-        max_bucket_occupancy = std::max(max_bucket_occupancy, (float) buckets[j].size());
+          if (debug) cout << kv << " ";
+        if (debug) cout << endl;
+        min_bucket_occupancy = min(min_bucket_occupancy, (float) buckets[j].size());
+        max_bucket_occupancy = max(max_bucket_occupancy, (float) buckets[j].size());
         avg_bucket_occupancy += buckets[j].size();
       }
       else {
-        if (debug) std::cout << " empty" << std::endl;
+        if (debug) cout << " empty" << endl;
         ++n_empty_buckets;
       }
     }
 
-    std::cout << "n buckets:" << buckets.size()
+    cout << "n buckets:" << buckets.size()
     << " entries:" << n_entries
     << " empty:" << n_empty_buckets
     << " min:" << min_bucket_occupancy
     << " avg:" << (avg_bucket_occupancy/(buckets.size() - n_empty_buckets))
     << " max:" << max_bucket_occupancy
-    << " hits:" << n_hits
-    << " len:" << search_length
-    << std::endl;
+    << " len:" << search_length // number of probes to reach a record or not present
+    << endl;
 
-    std::vector<float> dist((size_t)max_bucket_occupancy+1);
+    vector<float> dist((size_t)max_bucket_occupancy+1);
 
     for (Bucket bucket : buckets)
       ++dist[bucket.size()];
     float M = 0;
     for (size_t j = 0; j < dist.size(); ++j) {
       dist[j] /= (float) n_entries;
-      M = std::max(M, dist[j]);
+      M = max(M, dist[j]);
     }
-    std::cout << "\ndist= " << dist << std::endl << std::endl;
+    cout << "\ndist= " << dist << endl << endl;
     float k = 30 / M;
     for (size_t j = 0; j < dist.size(); ++j) {
       size_t l =  (size_t) (k * dist[j]);
-      std::cout << j << "\t:";
+      cout << j << "\t:";
       for (size_t jj = 0; jj < l; ++jj)
-        std::cout << "*";
-      std::cout << std::endl;
+        cout << "*";
+      cout << endl;
     }
   }
 
-  typedef std::vector<std::pair<K,V>> Bucket;
+  typedef vector<pair<K,V>> Bucket;
 
-  std::function<bool(size_t,size_t)> order = [&](size_t a, size_t b) {
-    return !((a < p && b >= p)
-           || (a < p && b < p && buckets[a].size() > buckets[b].size()));
+  function<bool(size_t,size_t)> order = [&](size_t a, size_t b) {
+    return buckets[a].size() < buckets[b].size();
+    //return !((a < p && b >= p)
+    //       || (a < p && b < p && buckets[a].size() > buckets[b].size()));
   };
 
-  std::vector<Bucket> buckets;
-  std::vector<size_t> priority;
-  size_t m; // initial number of buckets
-  size_t b; // bucket size
+  vector<Bucket> buckets;
+  vector<size_t> priority;
+  vector<bool> not_split;
+  const size_t m; // initial number of buckets
+  const size_t b; // bucket size
   size_t i; // splitting round number
-  size_t p; // bucket to split next
   size_t n_entries; // total number of entries in hash
-  size_t n_hits; // lucky hits where we split the right bucket
   float search_length;
   bool debug;
 };
